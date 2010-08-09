8b0d563aa43d0ca36c3497d8f01a03ce1880c671
AlphaGma <AlphaGma@c09a3246-3e14-e297-8869-fca169c7d2c1>
2010-08-09 08:24:17 +0000

    Moved from "WebIntruder" project.

---
+++ trunk/WebIntruder.Model.Plugin/Formats/ExportFormatBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebIntruder.Model.Plugin;
+using WebIntruder.Model.Plugin.Media;
+
+namespace WebIntruder.Model.Plugin.Formats
+{
+    public abstract class ExportFormatBase: PluginBase
+    {
+        public ExportFormatBase(ExportMediaBase exportMedia)
+        {
+            ExportMedia = exportMedia;
+        }
+
+        public ExportMediaBase ExportMedia { get; set; }
+
+        protected abstract string FormatResults(List<RequestResponseSnapshot> results);
+
+        public void Export(List<RequestResponseSnapshot> results)
+        {
+            var formattedResults = FormatResults(results);
+
+            ExportMedia.Export(formattedResults);
+        }
+    }
+}
---
+++ trunk/WebIntruder/FrmResults.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WebIntruder
+{
+    partial class FrmResults
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgResults = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblResultsTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgResults
+            // 
+            this.dgResults.AllowUserToAddRows = false;
+            this.dgResults.AllowUserToDeleteRows = false;
+            this.dgResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgResults.Location = new System.Drawing.Point(12, 25);
+            this.dgResults.Name = "dgResults";
+            this.dgResults.ReadOnly = true;
+            this.dgResults.Size = new System.Drawing.Size(643, 204);
+            this.dgResults.TabIndex = 1;
+            this.dgResults.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgResults_CellDoubleClick);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(580, 235);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(499, 235);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblResultsTitle
+            // 
+            this.lblResultsTitle.AutoSize = true;
+            this.lblResultsTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblResultsTitle.Name = "lblResultsTitle";
+            this.lblResultsTitle.Size = new System.Drawing.Size(45, 13);
+            this.lblResultsTitle.TabIndex = 0;
+            this.lblResultsTitle.Text = "Results:";
+            // 
+            // FrmResults
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 270);
+            this.Controls.Add(this.lblResultsTitle);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.dgResults);
+            this.Name = "FrmResults";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Results";
+            ((System.ComponentModel.ISupportInitialize)(this.dgResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgResults;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblResultsTitle;
+    }
+}
